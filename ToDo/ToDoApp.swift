368cb6448e2b38a42de8926415132928a197e2c9
EPedley <lizzypedley@gmail.com>
2024-04-09 11:01:02 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by LIZZY PEDLEY on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
